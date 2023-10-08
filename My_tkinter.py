71e3f30c9df97a3290dac8604e96afe592889cf4
Salil Debnath <114846592+salil-cloud@users.noreply.github.com>
2023-10-08 23:24:47 +0530

    Day 3 Ending

--- My_tkinter.py
+++ My_tkinter.py
@@ -3,8 +3,8 @@ from ttkbootstrap.constants import *
 import ttkbootstrap as tb
 
 root = tb.Window(themename='vapor')
-default_width = 1080
-default_height = 800
+default_width = 980
+default_height = 700
 
 # Set the default size
 root.geometry(f"{default_width}x{default_height}")
@@ -27,7 +27,7 @@ my_menu.pack(pady=10, padx=10, side=LEFT)
 inside_menu = tb.Menu(my_menu)  
 
 # Add content (a label) inside the frame
-my_title = tb.Label(upper_frame, text="File_Whiz Application", font=("Pacifico", 25, "bold", "italic"), bootstyle="info")
+my_title = tb.Label(upper_frame, text="File_Whiz Application", font=("Pacifico", 35, "bold", "italic"), bootstyle="info")
 my_title.pack(pady=5, padx=160, side=LEFT)
 
 # Load the image
@@ -76,18 +76,31 @@ def create_and_open_tab(section_name):
             directory_label = tb.Label(input_frame, text="__Enter File Path__:", font=('Arial', 12, 'bold', 'underline'), bootstyle="inverse-primary", relief="groove")
             directory_label.pack(side=LEFT, padx=10, pady=5)
             
+            # Function to save the directory path
+            user_directory = ""
+
+            def save_directory():
+                global user_directory
+                user_directory = directory_entry.get()
+                # You can now use the 'user_directory' variable for further processing
+
+            # Create an Entry widget to input the directory path
             directory_entry = tb.Entry(input_frame, font=('Arial', 12), bootstyle="light")
             directory_entry.pack(side=LEFT, fill=X, expand=True, padx=10, pady=10)
 
             # Button to save directory
             save_button = tb.Button(input_frame, text="SUBMIT", command=save_directory, bootstyle="danger")
             save_button.pack(side=LEFT, padx=20, pady=5)
+
             
             # Create two frames in the "Home" tab
             frame1 = tb.Frame(new_tab, bootstyle="light", relief=SUNKEN)
             frame2 = tb.Frame(new_tab, bootstyle="light", relief=SUNKEN)
+
+            #  Use the pack geometry manager to place the frames side by side
             frame1.pack(side=LEFT, fill=Y, expand=True, padx=10, pady=10)
             frame2.pack(side=RIGHT, fill=Y, expand=True, padx=10, pady=10)
+
 
             # Add content to the frames (you can customize this)
             label1 = tb.Label(frame1, text='______File counts by folder______', font=('Arial', 16, 'bold', 'underline'), bootstyle="inverse-primary", relief=RAISED)
@@ -95,6 +108,7 @@ def create_and_open_tab(section_name):
 
             label2 = tb.Label(frame2, text='____Organize files by folder____', font=('Arial', 16, 'bold', 'underline'), bootstyle='inverse-primary', relief=RAISED)
             label2.pack(pady=20, padx=10)
+            
 
 
 
